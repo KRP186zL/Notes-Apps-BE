84fcd8795455ceb34319668f8f6d535ec982b979
KRP186zL <krisnabonex5743@gmail.com>
2024-10-08 23:14:31 +0700

    Fix-Bugs-Host-Value-AWSxDicoding

--- src/server.js
+++ src/server.js
@@ -4,17 +4,16 @@ const routes = require('./routes');
 (async () => {
   const server = Hapi.server({
     port: 5000,
-    host:'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin:['*']
-      }
-    }
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
-
 })();
